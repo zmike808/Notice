ccacd47c9a10f9b23847fa858ea166805cffe2d8
Michael Zemsky <zmike808@gmail.com>
2014-02-15 06:37:36 -0500

    lets not be bad coders and use git so incase all shit goes to hell we can revert :D

---
+++ scrape_images.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import urllib
+import facebook
+from PIL import Image
+
+def setupParser():
+  parser = argparse.ArgumentParser(description="Scrape pictures from Facebook.")
+  parser.add_argument("node",
+      help="Facebook Graph node, used as the root node in our search")
+  parser.add_argument("token", help="Facebook API access token", type=str)
+  return parser.parse_args()
+
+def saveFriendPhotos(facebookId, graph, visitedPhotos):
+  foundPeople = set()
+  photos = graph.get_connections(facebookId, "photos", fields="images,tags")
+  for photo in photos["data"]:
+    if photo["id"] in visitedPhotos: continue
+    visitedPhotos.add(photo["id"])
+    # Download the photo from Facebook
+    originalPhoto = "original_photos/" + photo["id"] + ".jpg"
+    urllib.urlretrieve(photo["images"][0]["source"], originalPhoto)
+    for person in photo["tags"]["data"]:
+      try:
+        foundPeople.add(person["id"])
+        # Create person directory if it doesn't already exist
+        if not os.path.isdir("photos/" + person["id"]):
+          os.makedirs("photos/" + person["id"])
+        photoLocation = "photos/" + person["id"] + "/" + photo["id"] + ".jpg"
+        # Crop the photo so it only contains the face
+        image = Image.open(originalPhoto)
+        imageTagX = int(image.size[0] * person["x"]/100)
+        imageTagY = int(image.size[1] * person["y"]/100)
+        # TODO: We probably want to choose asubpicture size that varies by the
+        # original size
+        dimensions = (imageTagX-50, imageTagY-50, imageTagX+50, imageTagY+50)
+        area = image.crop(dimensions)
+        area.save(photoLocation, "jpeg")
+      except KeyError:
+        # Some tags seem to have missing information. I'm not sure why.
+        continue
+  return foundPeople
+
+def main():
+  args = setupParser()
+  graph = facebook.GraphAPI(args.token)
+  visitedPeople = set()
+  visitedPhotos = set()
+  foundPeople = saveFriendPhotos("me", graph, visitedPhotos)
+  # Walk one level deeper
+  for person in foundPeople:
+    saveFriendPhotos(person, graph, visitedPhotos)
+
+if __name__ == "__main__":
+  main()
